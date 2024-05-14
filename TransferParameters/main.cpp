4445f7650d2bc4648abf9dd2c5143174032cba5b
BV19-jeane <162352906+BV19-jeane@users.noreply.github.com>
2024-05-14 20:49:46 +0300

    Split DONE

--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int& a, int& b);
 
 void main()
 {
 	setlocale(LC_ALL, "Russian"); 
 	int a = 2, b = 3;
 	cout << a << "\t" << b<< endl;
+	cout << &a << "\t" << &b << endl;
+
 	Exchange(a, b);
 	cout << a << "\t" << b << endl;
 
 }
 
-void Exchange(int a, int b)
+void Exchange(int& a, int& b)
 {
+	cout << &a << "\t" << &b << endl;
 	int buffer = a;
 	a = b;
 	b = buffer;
